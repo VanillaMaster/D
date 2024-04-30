4a545d511fc84e401017f55f4a0f57caf852a38a
Vanilla <vanillaflamaster@gmail.com>
2024-04-30 18:34:45 +0300

    added @builtin/lit + updated package.json & jsconfig.json files

---
+++ README.md
@@ -0,0 +1,2 @@
+# D
+> TODO: write readme
--- builtin/ui/src/client.js
+++ builtin/ui/src/client.js
@@ -1,3 +1,25 @@
 import { client } from "@builtin/rpc/client";
 
 client.request("echo", { text: "Hello, World!" }).then(console.log);
+
+import { LitElement, css, html } from "@builtin/lit/client"
+
+class SimpleGreeting extends LitElement {
+    static styles = css`p { color: blue }`;
+
+    static properties = {
+        name: { type: String },
+    }
+
+    constructor() {
+        super();
+        this.name = 'Somebody';
+    }
+
+    render() {
+        return html`<p>Hello, ${this.name}!</p>`;
+    }
+}
+customElements.define('simple-greeting', SimpleGreeting);
+
+document.body.append(document.createElement("simple-greeting"));

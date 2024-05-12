ea9158649e551cc471b7fb369b68b7efebee315d
Vanilla <vanillaflamaster@gmail.com>
2024-05-12 13:16:16 +0300

    added sidebar

--- builtin/ui/src/client.js
+++ builtin/ui/src/client.js
@@ -49,6 +49,10 @@ class App extends LitElement {
             inset: 0;
             pointer-events: none;
         }
+        .overlay-container {
+            display: contents;
+            pointer-events: initial;
+        }
         /* :where(::slotted([slot=overlay])) {
             pointer-events: initial;
         } */
@@ -67,7 +71,9 @@ class App extends LitElement {
                 </div>
             </div>
             <div class="overlay">
-                <slot name="overlay"></slot>
+                <div class="overlay-container">
+                    <slot name="overlay"></slot>
+                </div>
             </div>
         `;
     }
@@ -164,7 +170,109 @@ class SideBar extends LitElement {
 
 customElements.define('builtin-sidebar', SideBar);
 
-{
+class SideBarItem extends LitElement {
+    static properties = {
+        active: {
+            type: Boolean,
+            reflect: true
+        }
+    };
+
+    constructor() {
+        super();
+
+        this.active = false;
+    }
+
+    static styles = css`
+        :host {
+            height: 56px;
+        }
+
+        :host([active]) .backdrop {
+            background-color: cadetblue;
+        }
+
+        button {
+            all: unset;
+            display: block;
+            height: 100%;
+            width: 100%;
+        }
+        .backdrop {
+            height: 100%;
+            width: 100%;
+
+            display: flex;
+            align-items: center;
+
+            font-family: sans-serif;
+            font-size: 14pt;
+            line-height: 20pt;
+            font-weight: 500;
+
+            padding: 0 24px 0 16px;
+            box-sizing: border-box;
+            border-radius: 9999px;
+        }
+    `;
+
+    onClick() {
+        this.active = true;
+        this.dispatchEvent(new Event("builtin-click", {
+            bubbles: true
+        }));
+    }
+
+    render(){
+        return html`
+            <button @click=${this.onClick}>
+                <div class="backdrop">
+                    <slot name="icon"></slot>
+                    <slot name="label"></slot>
+                </div>
+            </button>
+        `;
+    }
+}
+
+customElements.define('builtin-sidebar-item', SideBarItem);
+
+class SideBarList extends LitElement {
+
+    static styles = css`
+        :host {
+            display: flex;
+            flex-direction: column;
+        }
+    `;
+
+    constructor() {
+        super();
+        this.addEventListener("builtin-click", this.onClick, {
+            capture: true
+        });
+    }
+
+    /**
+     * @param { Event } e 
+     */
+    onClick(e) {
+        for (const element of this.children) {
+            if (!(element instanceof SideBarItem)) continue;
+            if (element !== e.target) element.active = false;
+        }
+    }
+
+    render(){
+        return html`<slot></slot>`;
+    }
+}
+
+customElements.define('builtin-sidebar-list', SideBarList);
+
+
+await (async function() {
     /**@param { Response } response */
     function text(response) {
         return response.text();
@@ -174,4 +282,4 @@ customElements.define('builtin-sidebar', SideBar);
     const template = document.createElement("template");
     template.innerHTML = html;
     document.body.append(template.content);
-}
+})();

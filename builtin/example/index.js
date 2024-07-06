ddca9a12632ad8983183600c9648a5bc20932d8e
VM <vanillaflamaster@gmail.com>
2024-07-06 15:42:36 +0300

    added example package

---
+++ builtin/example/index.js
@@ -0,0 +1,30 @@
+import "@builtin/ui/client"
+
+import { fetch as fetchProxy } from "@builtin/proxy/client"
+import { XMLSaxStream } from "@builtin/parsing/xml"
+import { fromNow } from "@builtin/ui/time"
+
+const response = await fetchProxy("https://habr.com/ru/rss/articles/?fl=ru");
+if (response.body == null) throw new Error();
+
+const container = /**@type { HTMLElement } */(document.getElementById("tmp-body"));
+const template = document.createElement("template");
+
+response.body.pipeThrough(new TextDecoderStream("utf-8")).pipeThrough(new XMLSaxStream(["item"])).pipeTo(new WritableStream({
+    write(node) {
+        const { children: { title: { text: title }, description: { text: description }, pubDate: { text: pubDate}, "dc:creator": { text: creator }, link: { text: link}} } = node;
+        template.innerHTML = `<builtin-feed-card>${description}</builtin-feed-card>`
+        const img = template.content.querySelector("img");
+        if (img == undefined) return;
+        const { src } = img;
+        template.innerHTML = `
+            <builtin-card>
+                <span slot="header" style="font-size: 1.2em;">${creator}</span>
+                <span slot="header" style="font-weight: 400;">${ fromNow(new Date(pubDate)) }</span>
+                <builtin-thumbnail style="height: 15em; border-radius: .5em; box-shadow: 0px 1px 2px 0px rgb(0 0 0 / 30%), 0px 1px 3px 1px rgb(0 0 0 / 15%);" src="${src}"></builtin-thumbnail>
+                <a slot="footer" href="${link}">${title}</a>
+            </builtin-card>
+        `;
+        container.append(template.content);
+    }
+}))
--- builtin/ui/src/client.js
+++ builtin/ui/src/client.js
@@ -7,9 +7,9 @@ import "./components/FeedCard.js"
 import "./components/Thumbnail.js"
 import "./components/Card.js"
 
-import { client } from "@builtin/communication/client";
+// import { client } from "@builtin/communication/client";
 
-client.request("echo", { text: "Hello, World!" }).then(console.log);
+// client.request("echo", { text: "Hello, World!" }).then(console.log);
 
 
 await (async function() {
@@ -23,34 +23,3 @@ await (async function() {
     template.innerHTML = html;
     document.body.append(template.content);
 })();
-
-import { fetch as fetchProxy } from "@builtin/proxy/client"
-import { XMLSaxStream } from "@builtin/parsing/xml"
-import { fromNow } from "./time.js"
-(async function() {
-
-    const response = await fetchProxy("https://habr.com/ru/rss/articles/?fl=ru");
-    if (response.body == null) throw new Error();
-    const container = /**@type { HTMLElement } */(document.getElementById("tmp-body"));
-    const template = document.createElement("template");
-    const promise = response.body.pipeThrough(new TextDecoderStream("utf-8")).pipeThrough(new XMLSaxStream(["item"])).pipeTo(new WritableStream({
-        write(node) {
-            // console.log(node);
-            const { children: { title: { text: title }, description: { text: description }, pubDate: { text: pubDate}, "dc:creator": { text: creator }, link: { text: link}} } = node;
-            template.innerHTML = `<builtin-feed-card>${description}</builtin-feed-card>`
-            const img = template.content.querySelector("img");
-            if (img == undefined) return;
-            const { src } = img;
-            template.innerHTML = `
-                <builtin-card>
-                    <span slot="header" style="font-size: 1.2em;">${creator}</span>
-                    <span slot="header" style="font-weight: 400;">${ fromNow(new Date(pubDate)) }</span>
-                    <builtin-thumbnail style="height: 15em; border-radius: .5em; box-shadow: 0px 1px 2px 0px rgb(0 0 0 / 30%), 0px 1px 3px 1px rgb(0 0 0 / 15%);" src="${src}"></builtin-thumbnail>
-                    <a slot="footer" href="${link}">${title}</a>
-                </builtin-card>
-            `;
-            container.append(template.content);
-            // else console.warn("no img");
-        }
-    }))
-})()
